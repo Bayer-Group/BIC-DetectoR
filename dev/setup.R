eb9000720165ab9e1fdd49a7a1fbc605bf0312a9
rh-bayer <ryan.harrison1.ext@bayer.com>
2026-06-03 15:19:58 +0000

    * Remove CODEOWNERS file * Update DESCRIPTION file * Update NEWS

--- dev/setup.R
+++ dev/setup.R
@@ -5,6 +5,15 @@
 # usethis::use_github_action(url = "https://github.com/posit-dev/setup-air/blob/main/examples/format-check.yaml")
 # usethis::use_github_action(url = "https://github.com/etiennebacher/setup-jarl/blob/main/examples/jarl-check.yml")
 
+
+# Snapshot
+renv::status()
+# renv::update(lock = TRUE)
+renv::snapshot(type = "explicit", dev = TRUE)
+
 spelling::update_wordlist()
+
+# Bump versions
+usethis::use_version("dev")
 
 rsconnect::writeManifest()
