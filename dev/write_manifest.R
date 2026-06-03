4e826ffaecdbafb0cb831236e019af0ae2ea1468
rh-bayer <ryan.harrison1.ext@bayer.com>
2026-06-03 15:07:47 +0000

    * Update WORDLIST * Update README * Add GHA for R CMD CHECK, air, jarl

--- dev/write_manifest.R
+++ dev/write_manifest.R
@@ -1,1 +1,10 @@
+# GitHub actions
+# usethis::use_github_action()
+
+# Add air GitHub actions
+# usethis::use_github_action(url = "https://github.com/posit-dev/setup-air/blob/main/examples/format-check.yaml")
+# usethis::use_github_action(url = "https://github.com/etiennebacher/setup-jarl/blob/main/examples/jarl-check.yml")
+
+spelling::update_wordlist()
+
 rsconnect::writeManifest()
